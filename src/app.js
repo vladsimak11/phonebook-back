b736cae224a3d46d6e077a68b8a5350f945af151
Vladyslav Simak <simakv21@gmail.com>
2024-04-14 12:11:07 +0300

    fix api

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use('/api/contacts', contactsRouter);
+app.use('/contacts', contactsRouter);
 app.use('/users', authRouter);
 
 app.use((req, res) => {
